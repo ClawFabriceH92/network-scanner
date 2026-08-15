378e5f7817f8105ac112f44dccbaf8b186684960
Fabrice <fabrice.heuvrard@gmail.com>
2026-08-15 14:49:20 +0200

    v1.5.0 : bump version (versionCode 22)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.fabrice.network.scanner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.4.2"
+        versionCode = 22
+        versionName = "1.5.0"
     }
 
     buildTypes {
